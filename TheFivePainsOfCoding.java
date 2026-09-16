830cf7e515b21f5fca05d3a19c0b9bdd3ade04e1
avyuktGupta52 <2021792@apps.nsd.org>
2026-09-16 13:02:35 -0700

    Create TheFivePainsOfCoding.java with class definition

---
+++ TheFivePainsOfCoding.java
@@ -0,0 +1,10 @@
+// Avyukt Gupta
+// AP Computer Science A
+// Period 6
+// The Five Pains of Coding
+
+public class TheFivePainsofCoding { // Define the class
+    public static void main(String[] args) { // main function: run everything inside this
+        
+    }
+}
